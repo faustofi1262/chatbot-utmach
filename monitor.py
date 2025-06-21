be5d399fc1764b6582581a142f63ad34ab799fd0
Fausto Figueroa <faustofi1262@yahoo.com>
2025-06-20 21:29:25 -0500

    una linea en monitor.py cambie el puerto

--- monitor.py
+++ monitor.py
@@ -153,4 +153,4 @@ def registrar_usuario():
         return jsonify({"error": f"❌ Error al registrar usuario: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8083, debug=False)
+   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
