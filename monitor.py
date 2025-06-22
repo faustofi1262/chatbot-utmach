60c7b3b38a526bdce9f4a278c2c838f8b81474be
Fausto Figueroa <faustofi1262@yahoo.com>
2025-06-22 18:57:39 -0500

    una linea en monitor.py cambie el puerto

--- monitor.py
+++ monitor.py
@@ -26,6 +26,7 @@ db = psycopg2.connect(
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     dbname=os.getenv("DB_DATABASE")
+    sslmode="require"
 )
 cursor = db.cursor()
 
